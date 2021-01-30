ebc8e17f5202855006388be8e6e6fe4ca882282e
J-vInyL <wnstjq7894@gmail.com>
2021-01-30 21:21:17 +0900

    App.js 부분 오류 수정 완료

--- App.js
+++ App.js
@@ -1,44 +1,55 @@
-import React from 'react';
-import Loading from './Loading';
-import * as Location from 'expo-location';
-import { Alert } from 'react-native';
-import axios from 'axios';
+import React from "react";
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import {Alert} from "react-native";
+import axios from "axios";
+import Weather from "./Weather";
 
-const API_KEY = '682921757479b7e40b8f6f77151397d4';
+const API_KEY = "682921757479b7e40b8f6f77151397d4";
 
 export default class extends React.Component {
-    state = {
-        isLoading: true,
-    };
+  state = {
+    isLoading: true
+  };
 
-    getWeather = async (latitude, longitude) => {
-        const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`);
-        //openweathermap 에서 API 가져오기  ( 경도와 위도 사용 )
-        // data 값을 이용하여 위치에 맞는 날씨 보여주기
-        // axios란 = Axios는 브라우저, Node.js를 위한 Promise API를 활용하는 HTTP 비동기 통신 라이브러리입니다.
-        //쉽게 말해서 백엔드랑 프론트엔드랑 통신을 쉽게하기 위해 Ajax와 더불어 사용합니다.
-        console.log(data);
-    };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: {temp},
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    //openweathermap 에서 API 가져오기  ( 경도와 위도 사용 )
+    // data 값을 이용하여 위치에 맞는 날씨 보여주기
+    // axios란 = Axios는 브라우저, Node.js를 위한 Promise API를 활용하는 HTTP 비동기 통신 라이브러리입니다.
+    //쉽게 말해서 백엔드랑 프론트엔드랑 통신을 쉽게하기 위해 Ajax와 더불어 사용합니다.
+    this.setState({isLoading: false, condition: weather[0].main, temp});
+  };
 
-    getLocation = async () => {
-        try {
-            await Location.requestPermissionsAsync();
-            const {
-                coords: { latitude, longitude },
-            } = await Location.getCurrentPositionAsync();
-            this.getWeather(latitude, longitude);
-            this.setState({ isLoading: false });
-        } catch (error) {
-            Alert.alert("Can't find you.", 'So sad');
-        }
-    };
-
-    componentDidMount() {
-        this.getLocation();
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: {latitude, longitude}
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude); //만약 this.setstate(isLoading:false)이 있었다면 setstate가 발생할때마다 새로운 render가 되기떄문에 오류가 발생함.
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
     }
+  };
 
-    render() {
-        const { isLoading } = this.state;
-        return isLoading ? <Loading /> : null;
-    }
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const {isLoading, temp, condition} = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
+  }
 }
